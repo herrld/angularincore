c8e412d5702938e9fd27564f87c0a684ad294827
sniffles <8985875+herrld@users.noreply.github.com>
2019-04-06 15:19:13 -0500

    initial

---
+++ DutchTreat/DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController: Controller
+    {
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<string> Get()
+        {
+            try
+            {
+                return Ok("stuff");
+            }
+            catch( Exception ex)
+            {
+                return BadRequest("its bad mkay");
+            }
+        }
+    }
+}
---
+++ DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddJsonOptions(opt =>  opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+            services.AddAuthentication()
+                .AddOpenIdConnect();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //env.IsEnvironment("Development")
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg => 
+            {
+                cfg.MapRoute("Default",
+                    "/{controller}/{action}/{id?}", 
+                    new {
+                        controller = "App",
+                        Action = "index"
+                    });
+            });
+        }
+    }
+}
